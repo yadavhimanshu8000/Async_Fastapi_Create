6b94e272bfdcb743a08ea221fe04e75c91c63003
himanshu <singhhimanshu8000@gmail.co>
2025-06-13 16:28:14 +0530

    some change

--- database/db.py
+++ database/db.py
@@ -1,7 +1,7 @@
 from sqlalchemy.ext.asyncio import AsyncSession, create_async_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
 
-DATABASE_URL = "postgresql+asyncpg://postgres:himanshuapi@localhost:7777/userdetails"
+DATABASE_URL = "postgresql+asyncpg://postgres:himanshuapi@localhost:5432/userdetails"
 
 engine = create_async_engine(DATABASE_URL, echo=True)
 SessionLocal = sessionmaker(bind=engine, class_=AsyncSession, expire_on_commit=False)
